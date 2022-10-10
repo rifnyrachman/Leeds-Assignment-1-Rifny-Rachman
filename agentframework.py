c807b93ba7cc3a9936536edf0748c09af075dbde
rifnyrachman <rifny.rachman@gmail.com>
2022-10-10 17:30:27 +0100

    fixing bug in the python files
    The code was unintentionally doubled. It has been fixed here

--- agentframework.py
+++ agentframework.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-# -*- coding: utf-8 -*-
 """
 Created on Fri Oct  7 04:41:45 2022
 
@@ -23,30 +21,4 @@ class Agent():
         if random.random()<0.5:
             self.y = (self.y+1)%100
         else:
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  7 04:41:45 2022
-
-@author: rifny
-"""
-
-#class, defining agents as abstract data type
-import random
-
-class Agent():
-    def __init__(self):
-        self.x = random.randint(0,100)
-        self.y = random.randint(0,100)
-        print(self.x,self.y)
-    
-    def move(self):
-        if random.random()<0.5:
-            self.x = (self.x+1)%100
-        else:
-            self.x = (self.x-1)%100
-        if random.random()<0.5:
-            self.y = (self.y+1)%100
-        else:
->>>>>>> 70245d226c0df4bf0768263fcf42c7058813af5b
             self.y = (self.y-1)%100 
