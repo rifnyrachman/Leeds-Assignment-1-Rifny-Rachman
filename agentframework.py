c3b1d4df188a00a2b541a77455cd59190aa929a6
rifnyrachman <rifny.rachman@gmail.com>
2022-10-23 18:27:23 +0100

    Add shuffle function, debug wolf and breeding function

--- agentframework.py
+++ agentframework.py
@@ -4,10 +4,11 @@ Created on Fri Oct  7 04:41:45 2022
 @author: rifny
 """
 
-#class, defining agents as abstract data type
 import random
 
+#class, defining agents as an abstract data type
 class Agent():
+    #initiating the variables in Agent class
     def __init__(self,i,agents,environment,y,x):
         self.store = 0
         self.i = i
@@ -15,17 +16,19 @@ class Agent():
         self.environment = environment
         self.x = x
         self.y = y
-        # self.x = random.randint(0,99)
+        # self.x = random.randint(0,99) #uncomment to generate random number
         # self.y = random.randint(0,99)
-        
+    
+    #bypass the agents function to return string as follows    
     def __str__(self):
         return "agent-" + str(self.i) + " ,store = " + str(self.store) \
             + " ,position(x y) :" + str(self.x) + " " + str(self.y)
-    
+            
+    #create fucntion to move agents
     def move(self):
         #Agents move randomly by a step
         if random.random()<0.5:
-            self.x = (self.x+1)%100
+            self.x = (self.x+1)%100 #any value more than 100 would be subtracted by 100
         else:
             self.x = (self.x-1)%100
         if random.random()<0.5:
@@ -36,7 +39,7 @@ class Agent():
         #Agents move quicker (by +/-5 steps) if they have more resources (>60)
         if self.store > 60:
             if random.random()<0.5:
-                self.x = (self.x+5)%100
+                self.x = (self.x+5)%100 #make sure value is no more than 100
             else:
                 self.x = (self.x-5)%100
             if random.random()<0.5:
@@ -47,7 +50,7 @@ class Agent():
     
     # agents eat from the environment to add their stores
     def eat(self):
-        #each agent each 10 from the environment
+        #each agent eats 10 from the environment
         if self.environment[self.y][self.x] > 10:
             self.environment[self.y][self.x] -= 10
             self.store += 10
@@ -61,34 +64,36 @@ class Agent():
             self.environment[self.y][self.x] += self.store
             self.store = 0
         
-        
+    #create function to calculate distance between the agent and another agent
     def distance_between(self,agents_row_b):
         return ((self.x-agents_row_b.x)**2+(self.y-agents_row_b.y)**2)**0.5
             
+    #create function to share agent's store with its neighbour
     def share_with_neighbourhoods(self,neighbourhood):
         for i in range(len(self.agents)):
             distance = self.distance_between(self.agents[i])
-            #each agent share the store with its neighbour
-            if distance <= neighbourhood:
+            #each agent equally shares the store with its neighbour
+            if (distance <= neighbourhood):
                 ave = (self.agents[i].store + self.store)/2
                 self.agents[i].store = ave
                 self.store = ave
-                # activate below code to see how much each agent shares to its neighbour
+                #uncomment below code to see how much each agent shares to its neighbour
                 #print("Agent-", i, "sharing: ", ave, ", distance: ",distance)
+            else:
+                pass
                 
-            #steal more resources from others if they are low
+            #agent steals more resources from others if they are low
             if (self.store < 40) and (self.agents[i].store > 40):
                 self.store += 20
                 self.agents[i].store -= 20
                 
     def wolves(self,wolf_x,wolf_y):
-        # wolf_x=random.randint(0,99)
-        # wolf_y=random.randint(0,99)
+        #calculate agent's distance with the wolf
         wolf_dist = ((wolf_x-self.x)**2+(wolf_y-self.y)**2)**0.5
-        if (wolf_dist < 50):
+        if (wolf_dist < 50): #wolf eats agent if the distance is less than 50
             print("eaten by wolf")
-            del self.agents #delete eaten agents
-            self.store = -99
+            del self.agents # to delete eaten agents from agents list
+            self.store = -99 #as a penalty in case del function does not work
             self.x = 0
             self.y = 0
             
--- model.py
+++ model.py
@@ -43,22 +43,21 @@ with open('in.txt', newline='') as f:
     for row in dataset:
         rowlist=[]
         for value in row:
-            #print(value)
+            #print(value) #uncomment to print file
             rowlist.append(value)
         environment.append(rowlist)
 
-#create agent list
+#initiate agent list setup
 agents=[]
 num_of_agents = 30
 num_of_iterations = 100
 neighbourhood = 20
 
-fig = matplotlib.pyplot.figure(figsize=(7, 7)) #initialize animation
+fig = matplotlib.pyplot.figure(figsize=(7, 7)) #initiate animation
 ax = fig.add_axes([0, 0, 1, 1])
 
 #Generate each agent's position within agent list using iteration
 #Agent's positions is based on the html data
-
 for i in range(num_of_agents):
     #in case the number of agents is more than what is provided in html data,
     #it generates random number for the rest
@@ -70,11 +69,23 @@ for i in range(num_of_agents):
         y = int(td_ys[i].text)
     else:
         y = random.randint(0,99)
+    #add generated value from html file to agents list
     agents.append(agentframework.Agent(i,agents,environment,y,x))
-    
+
+#create agents status function    
 def init_position():    
-    print("\n===============================================\nAgents' Positions:\n===============================================\n")
-    for i in range(num_of_agents):
+    print("\n===============================================\nAgents' Status:\n===============================================\n")
+    for i in range(len(agents)):
+        if (agents[i].store >= 0):
+            print(agents[i])
+        else:
+            pass
+
+#create agents shuffled status function
+def shuffle_agents():
+    print("\n===============================================\nShuffled Agents' Status:\n===============================================\n")
+    random.shuffle(agents)
+    for i in range(len(agents)):
         print(agents[i])
     
 #Test the communication between agents by printing agent-1 from agent-0
@@ -82,24 +93,22 @@ def com_test():
     print("\n===============================================\nCommunication test:\nCalling from agent-0:\n===============================================\n ",
           agents[0].agents[1])
 
+#Create distance function
 def print_distance():
-    dist=[]
+    dist=[] #create distance list to enable maximum and minimum distance
     print("\n===============================================\nDistances Between 2 Agents:\n===============================================\n")
-    for i in range(num_of_agents):
-        for j in range(num_of_agents):
+    for i in range(len(agents)):
+        for j in range(len(agents)):
             if (i!=j and i<j) and (agents[i].store >= 0):
                 distance_between = ((agents[i].x-agents[j].x)**2+(agents[i].y-agents[j].y)**2)**0.5
                 print("agent-",i,"and agent-",j,"is:",round(distance_between,2))
                 dist.append(distance_between)
             else:
                 pass
-                # print("agent-",i,"and agent-",j,"is: N/A (agent's been eaten)")
     
 #show max and min distances
     print("\nmax distance:",round(max(dist),2))
     print("min distance:",round(min(dist),2))  
-
-#random.shuffle(agents) #to shuffle the agents representation
 
 #create animation function to attach in GUI "Run Model" button
 def run():
@@ -113,9 +122,9 @@ def update(frame_number):
     fig.clear()
     global carry_on
     
-    for i in range(num_of_agents):
-        #set agents' behaviour
-        #Creating stopping condition when agent's store >= 80
+    #Create function that reflects agents behaviour
+    for i in range(len(agents)):
+        #Creating iteration stopping condition when agent's store >= 80
         if agents[i].store < 80:
             agents[i].move()
             agents[i].eat()
@@ -126,7 +135,7 @@ def update(frame_number):
         else:
             carry_on = False
             
-    #Creating random stopping condition
+    #Uncomment to create random stopping condition
     # if random.random() < 0.1:
     #     carry_on = False
     #     print("stopping condition")
@@ -135,11 +144,12 @@ def update(frame_number):
     plt.xlim(0,99)
     plt.ylim(0,99)
     plt.imshow(environment)
-    for i in range(num_of_agents):
+    for i in range(len(agents)):
         plt.scatter(agents[i].x,agents[i].y)
     #plt.show()
     canvas.draw()
     
+    #print stopping condition
     if agents[i].store >= 80:
         print("\n ///////Stopping Condition at frame-", frame_number,"///////")
     else:
@@ -148,7 +158,7 @@ def update(frame_number):
 #Create frames for animation
 def gen_function(b = [0]):
     a = 0
-    global carry_on #Not actually needed as we're not assigning, but clearer
+    global carry_on #Not actually needed as we are not assigning, but clearer
     while (a < 20) & (carry_on) :
         yield a			# Returns control and waits next call.
         a = a + 1
@@ -156,7 +166,7 @@ def gen_function(b = [0]):
 #Create function for wolves that eat some nearest sheeps (agents)        
 def print_wolves():
     print("\n===============================================\nAdding Wolves\n===============================================\n")
-    wolf_x=random.randint(0,99)
+    wolf_x=random.randint(0,99) #generate random wolf position
     wolf_y=random.randint(0,99)
     print("Wolf's position (x y) : ",wolf_x,wolf_y)
     for i in range(len(agents)):
@@ -167,24 +177,17 @@ def breed():
   init_position()
   print("===============================================\nBreeding Agents\n===============================================")
   print("Additional agents after breeding:")
-  for i in range(30,51):
-      x = random.randint(0,99)
+  for i in range(30,51): #create additional 20 agents
+      x = random.randint(0,99) #generate random agents position
       y = random.randint(0,99)
       agents.append(agentframework.Agent(i,agents,environment,y,x))
+      #Add new agents' behaviour
+      if agents[i].store < 80:
+          agents[i].move()
+          agents[i].eat()
+          agents[i].share_with_neighbourhoods(neighbourhood)
       print(agents[i])        
-      num_of_agents = i
         
-#calling animation module
-# animation = matplotlib.animation.FuncAnimation(
-#     fig, update, interval=1, repeat=False, frames=num_of_iterations)
-#animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False)
-
-#print the latest agents' position
-# for i in range(num_of_agents):  
-#     print(agents[i])
-        #print("Agent-",i,":",agents[i].x,agents[i].y)
-
-
 #Create a csv file as an output from the program
 with open('dataout.csv', 'w', newline='') as f2:
     writer = csv.writer(f2, delimiter=',')     
@@ -201,7 +204,8 @@ root.config(menu=menu)
 model_menu = tkinter.Menu(menu)
 agents_menu = tkinter.Menu(menu)
 menu.add_cascade(label="Agents", menu=agents_menu)
-agents_menu.add_command(label="Position", command=init_position)
+agents_menu.add_command(label="Status", command=init_position)
+agents_menu.add_command(label="Shuffled status", command=shuffle_agents)
 agents_menu.add_command(label="Distance", command=print_distance) 
 agents_menu.add_command(label="Communication Test", command=com_test)
 menu.add_cascade(label="Model", menu=model_menu)
